ae955ca9fc6e8dfccc5787a465988967111b8fb3
Tomáš Kukrál <tomkukral@users.noreply.github.com>
2018-01-17 14:09:37 +0100

    brg

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
-func readConfig() (string, string) {
+func config() (string, string) {
 	url := os.Getenv("ARCHITECT_INVENTORY_API_URL")
 	if url == "" {
 		url = "https://localhost:8181"
@@ -19,15 +23,54 @@ func readConfig() (string, string) {
 	return url, inv
 }
 
-func main() {
-	var apiUrl, inv string
+func args() string {
+	flag.Parse()
 
-	apiUrl, inv = readConfig()
+	if len(flag.Args()) < 1 {
+		log.Fatal("Missing resource name parameter")
+	}
 
-	url := fmt.Sprintf(
-		"%s/inventory/v1/%s/data.json?source=%s",
-		apiUrl, inv, "hovno",
+	return flag.Arg(0)
+}
+
+func getUrl(apiUrl, inv, resourceName string) string {
+	return "https://api.chucknorris.io/jokes/random"
+
+	return fmt.Sprintf(
+		"%s/inventory/v1/%s/%s/data.json?source=%s",
+		apiUrl, inv, resourceName, "salt-pillar",
 	)
 
-	fmt.Println(url)
+	// resource names: ansible-inventory, salt-pillar, salt-top
+}
+
+func main() {
+	var apiUrl, inv, resourceName string
+
+	apiUrl, inv = config()
+	resourceName = args()
+	url := getUrl(apiUrl, inv, resourceName)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s\n", body)
+
 }
